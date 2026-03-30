71f7a0a85cd0c907376186380c3d1f1814d2f415
Nadun Malwenna <107237482+HewageNKM@users.noreply.github.com>
2026-03-30 18:40:22 +0530

    refactor: migrate next.config.mjs to next.config.ts and add serverExternalPackages configuration

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,29 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  serverExternalPackages: ["@tensorflow/tfjs-node", "@mapbox/node-pre-gyp"],
   reactCompiler: true,
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // UPDATE THIS IN PRODUCTION
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization" },
+        ]
+      }
+    ]
+  }
 };
 
 export default nextConfig;
